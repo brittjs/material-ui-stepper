405e2788a752249547d0fbad3f5f781dbbfeea63
BTL <brittanyjsee@gmail.com>
2016-07-15 10:25:53 -0700

    refactor to tabs

--- index.js
+++ index.js
@@ -3,32 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Stepper = exports.StepLabel = exports.StepContent = exports.StepButton = exports.Step = undefined;
+exports.Tab = exports.Tabs = undefined;
 
-var _Step2 = require('./Stepper/Step');
+var _Tabs2 = require('./Tabs');
 
-var _Step3 = _interopRequireDefault(_Step2);
+var _Tabs3 = _interopRequireDefault(_Tabs2);
 
-var _StepButton2 = require('./Stepper/StepButton');
+var _Tab2 = require('./Tabs/Tab');
 
-var _StepButton3 = _interopRequireDefault(_StepButton2);
-
-var _StepContent2 = require('./Stepper/StepContent');
-
-var _StepContent3 = _interopRequireDefault(_StepContent2);
-
-var _StepLabel2 = require('./Stepper/StepLabel');
-
-var _StepLabel3 = _interopRequireDefault(_StepLabel2);
-
-var _Stepper2 = require('./Stepper/Stepper');
-
-var _Stepper3 = _interopRequireDefault(_Stepper2);
+var _Tab3 = _interopRequireDefault(_Tab2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Step = _Step3.default;
-exports.StepButton = _StepButton3.default;
-exports.StepContent = _StepContent3.default;
-exports.StepLabel = _StepLabel3.default;
-exports.Stepper = _Stepper3.default;
+exports.Tabs = _Tabs3.default;
+exports.Tab = _Tab3.default;
